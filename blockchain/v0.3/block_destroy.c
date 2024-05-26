5e54f8a15a78ae9434d5c6d90e002efce284d604
Adura-Hephzibah <adurahephzibah@gmail.com>
2024-05-26 16:18:00 +0200

    commit

---
+++ blockchain/v0.3/block_destroy.c
@@ -0,0 +1,12 @@
+#include "blockchain.h"
+
+/**
+ * block_destroy - Frees a block and its allocated memory
+ * @block: Pointer to the block to be destroyed
+ */
+
+void block_destroy(block_t *block)
+{
+	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	free(block);
+}
